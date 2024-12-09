6fd543711f49460ff480ba5844c5f845d397892f
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-12-09 20:38:15 +0300

    add: добавила ползунок для корзины

--- src/layout/cart/cart.jsx
+++ src/layout/cart/cart.jsx
@@ -3,16 +3,25 @@ import Empty from "./empty/empty";
 import ExitSvg from "../../assets/svg/exitSvg";
 import { Button } from "../../components/button/button";
 import CardCloseSvg from "../../assets/svg/cardCloseSvg";
-import styles from "./cart.module.sass";
+
 import SliderCart from "./sliderCart/sliderCart";
 
-const Cart = ({ setIsCartOpen, isCartOpen, cartItems, setIsVisibles, setCartItems }) => {
+import styles from "./cart.module.sass";
+const Cart = ({
+  setIsCartOpen,
+  isCartOpen,
+  cartItems,
+  setIsVisibles,
+  setCartItems,
+}) => {
   const totalSum = cartItems.reduce(
     (sum, { product, count }) => sum + product.price * count,
     0
   );
   const removeFromCart = (productId) => {
-    const updatedCart = cartItems.filter(({ product }) => product.id !== productId);
+    const updatedCart = cartItems.filter(
+      ({ product }) => product.id !== productId
+    );
     setCartItems(updatedCart);
   };
   return (
@@ -28,56 +37,71 @@ const Cart = ({ setIsCartOpen, isCartOpen, cartItems, setIsVisibles, setCartItem
           styles.cart__wrapper + " " + (isCartOpen ? styles.open : styles.close)
         }
       >
-        <div
-          className={styles.exitCart_svg}
-          onClick={() => setIsVisibles(false)}
-        >
-          <ExitSvg />
-        </div>
-        <h1 className={styles.cart_title}>КОРЗИНА</h1>
+        {cartItems.length !== 0 ? (
+          <>
+            <div
+              className={styles.exitCart_svg}
+              onClick={() => setIsVisibles(false)}
+            >
+              <ExitSvg />
+            </div>
+            <h1 className={styles.cart_title}>КОРЗИНА</h1>
+          </>
+        ) : (
+          ""
+        )}
+
         {cartItems.length === 0 ? (
           <Empty />
         ) : (
-          cartItems.map(({ product, count }) => (
-            <Link to={`/product/${product.id}`} key={product.id}>
-              <div className={styles.cartItems}>
-                <div className={styles.cart_img}>
-                  <img
-                    src={product.images}
-                    alt={product.name}
-                    className={styles.prod_image}
-                  />
+          <div className={styles.cart_scroll}>
+            {cartItems.map(({ product, count }) => (
+              <Link to={`/product/${product.id}`} key={product.id}>
+                <div className={styles.cartItems}>
+                  <div className={styles.cart_img}>
+                    <img
+                      src={product.images}
+                      alt={product.name}
+                      className={styles.prod_image}
+                    />
+                  </div>
+
+                  <div className={styles.cart_discr}>
+                    <p className={styles.prod_name}>
+                      {product.name}: {count}
+                    </p>
+                    <div
+                      className={styles.close_svg}
+                      onClick={() => removeFromCart(product.id)}
+                    >
+                      <CardCloseSvg />
+                    </div>
+                    <p className={styles.prod_price}>{product.price}₽</p>
+                  </div>
                 </div>
-               
-                <div className={styles.cart_discr}>
-                  <p className={styles.prod_name}>
-                    {product.name}: {count}
-                  </p>
-                  <div className={styles.close_svg} onClick={() => removeFromCart(product.id)}>
-                  <CardCloseSvg />
-                </div>
-                  <p className={styles.prod_price}>{product.price}₽</p>
-                </div>
-              </div>
-            </Link>
-          ))
+              </Link>
+            ))}
+          </div>
         )}
-        <div className={styles.sum}>
-          <p className={styles.sum_text}>
-            Сумма заказа: <b>{totalSum}₽</b>
-          </p>
-          <p className={styles.sum_text}>
-            Стоимость доставки: <b>бесплатно</b>
-          </p>
-          <p className={styles.sum_check}>
-            К оплате: <b>{totalSum}₽</b>
-          </p>
-        </div>
-        <Button>Оформить заказ</Button>
+        {cartItems.length !== 0 && (
+          <>
+            <div className={styles.sum}>
+              <p className={styles.sum_text}>
+                Сумма заказа: <b>{totalSum}₽</b>
+              </p>
+              <p className={styles.sum_text}>
+                Стоимость доставки: <b>бесплатно</b>
+              </p>
+              <p className={styles.sum_check}>
+                К оплате: <b>{totalSum}₽</b>
+              </p>
+            </div>
+            <Button>Оформить заказ</Button>
+          </>
+        )}
       </div>
-      <SliderCart/>
+      <SliderCart />
     </div>
-    
   );
 };
 
