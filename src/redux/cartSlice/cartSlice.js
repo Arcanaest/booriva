1700ee3b47f24e83e597c9566391982477557e60
ElzaEldarh <bbheldarhanova2201@mail.ru>
2024-12-28 14:57:48 +0300

    feat: создала хранилище для состояний Cart

---
+++ src/redux/cartSlice/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cartItems: localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [],
+    isCartOpen: false
+  },
+  reducers: {
+    setCartItems: (state, { payload }) => {
+      state.cartItems = payload;
+    },
+    setIsCartOpen: (state, { payload }) => {
+      state.isCartOpen = payload;
+    },
+  },
+});
+
+
+export const { setCartItems } = cartSlice.actions;
+export const {setIsCartOpen}= cartSlice.actions;
+
+export default cartSlice.reducer;
+
