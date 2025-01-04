3d204cf5756c328459f3dcd830a0da82953f9a33
ElzaEldarh <bbheldarhanova2201@mail.ru>
2025-01-05 02:18:05 +0300

    n

--- src/pages/catalogPage/subcategories.jsx
+++ src/pages/catalogPage/subcategories.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import styles from "./catalogPage.module.sass";
 
-const Subcategories = ({ subcategories, activeSubcategory, toNavigate, categoryName }) => {
+const Subcategories = ({
+  subcategories,
+  activeSubcategory,
+  toNavigate,
+  categoryName,
+}) => {
   return (
     <div className={styles.subcategoryWrapper}>
-    {categoryName !== "Новинки" && (
-      <div className={styles.filters}>
-        <div className={styles.subcategories}>
-        <h3 className={styles.left__side__title}>
-            ПОДКАТЕГОРИИ:
-          </h3>
-          <div className={styles.title__items}>
-            {subcategories.map((subcategory) => (
-              <div
-                onClick={() => toNavigate(subcategory.id)}
-                key={subcategory.id}
-                className={`${styles.left__side__item} ${
-                  activeSubcategory === subcategory.id
-                    ? styles.active
-                    : ""
-                }`}
-              >
-                {subcategory.name}
-              </div>
-            ))}
+      {categoryName !== "Новинки" && subcategories.length > 0 && (
+        <div className={styles.filters}>
+          <div className={styles.subcategories}>
+            <h3 className={styles.left__side__title}>ПОДКАТЕГОРИИ:</h3>
+            <div className={styles.title__items}>
+              {subcategories.map((subcategory) => (
+                <div
+                  onClick={() => toNavigate(subcategory.id)}
+                  key={subcategory.id}
+                  className={`${styles.left__side__item} ${
+                    activeSubcategory === subcategory.id ? styles.active : ""
+                  }`}
+                >
+                  {subcategory.name}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </div>
+      )}
+    </div>
   );
 };
 
